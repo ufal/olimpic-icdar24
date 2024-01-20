4c54fddc6b99c6f05e8096f7068ed04560565e75
Jirka-Mayer <jirkamayer97@gmail.com>
2024-01-20 15:23:28 +0100

    Updated slurm scripts

--- slurm/build_dataset_synthetic_02.sh
+++ slurm/build_dataset_synthetic_02.sh
@@ -1,11 +1,11 @@
 SLICE_COUNT=50
 
-.venv/bin/python3 -m app.datasets.synthetic_01 clear
+.venv/bin/python3 -m app.datasets.synthetic_02 clear
 
 # THE xvfb-run IS IMPORTANT FOR MUSESCORE! (virtual X-Server)
 # -a means it will allocate a different server number if 99 is taken
 # (which happens when there are more tasks on one worker machine)
-CMD="xvfb-run -a .venv/bin/python3 -m app.datasets.synthetic_01 build --slice_count $SLICE_COUNT --slice_index \$SLURM_PROCID"
+CMD="xvfb-run -a .venv/bin/python3 -m app.datasets.synthetic_02 build --slice_count $SLICE_COUNT --slice_index \$SLURM_PROCID"
 srun -u -n $SLICE_COUNT -p cpu-ms bash -c "$CMD"
 
-.venv/bin/python3 -m app.datasets.synthetic_01 finalize
+.venv/bin/python3 -m app.datasets.synthetic_02 finalize
